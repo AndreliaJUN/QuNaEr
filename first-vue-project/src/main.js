e374dc90ea1ad32b16affeef816caaa43fc82f84
AndreliaJUN <2215731376@qq.com>
2020-05-11 21:58:16 +0800

    commit swiper

--- first-vue-project/src/main.js
+++ first-vue-project/src/main.js
@@ -3,14 +3,18 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import 'styles/reset.css'
 import 'styles/border.css'
 import 'styles/iconfont.css'
+import 'swiper/dist/css/swiper.css'
 import fastclick from 'fastclick'
+
 
 Vue.config.productionTip = false
 fastclick.attach(document.body)
 /* eslint-disable no-new */
+Vue.use(VueAwesomeSwiper)
 new Vue({
   el: '#app',
   router,
---
+++ first-vue-project/src/pages/home/components/Swiper.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="wrapper">
+     <swiper :options="swiperOptions">
+    <swiper-slide v-for="item of swiperList"  :key="item.id">
+       <img  class="swiper-img" :src="item.imgUrl" />
+    </swiper-slide>
+    <div class="swiper-pagination" slot="pagination"></div>
+  </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'HomeSwiper',
+  data () {
+      return {
+          swiperOptions:{
+              pagination:'.swiper-pagination',
+              loop:true
+          },
+          swiperList:[{
+              id:'0001',
+              imgUrl:'https://imgs.qunarzz.com/vs_ceph_vs_vacation_pm/e4fd6ba9-84ad-4b6d-a73a-15298e8c3fcd.jpg'
+          }, {
+              id:'0002',
+              imgUrl:'https://img1.qunarzz.com/qs/2001/d9/8844a335554e1c02.jpg'
+          }]
+      }
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+.wrapper >>> .swiper-pagination-bullet-active 
+  background:#fff
+.wrapper
+ width:100%
+ height:31.25vw
+ background:#eee
+ .swiper-img
+   width:100%
+</style>
